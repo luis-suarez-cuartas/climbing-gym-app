df8dd8a290f2b38009280e0ccaf3db38f91a85bd
luis-suarez-cuartas <luissuarezcuartas@opendeusto.es>
2024-08-23 18:34:31 +0200

    preparacion del frontend para desplegar app en heroku

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from decouple import config
+
 
 
 
@@ -24,12 +26,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l^%nxn7+e!zs$#o3s%3(t0+#z2d0zvu+x^)xkgaj0*s#zz4#yq'
+#SECRET_KEY = 'django-insecure-l^%nxn7+e!zs$#o3s%3(t0+#z2d0zvu+x^)xkgaj0*s#zz4#yq'
+
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='unsafe-secret-key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -99,10 +104,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': dj_database_url.config(
-        default='mysql://USER:PASSWORD@HOST:PORT/NAME'
+        default='mysql://bc5d90835768a:ec23af62qu5@us-cluster-east-01.k8s.cleardb.net/heroku_caf1501aflac492?reconnect=true'
     )
 }
 
@@ -140,9 +144,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '..', 'frontend', 'myapp', 'build', 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
