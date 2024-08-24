95d34a6efcaba40766ffeac1c33b794d537fc7f7
luis-suarez-cuartas <luissuarezcuartas@opendeusto.es>
2024-08-24 19:09:41 +0200

    Usar root del backend

--- backend/myproject/wsgi.py
+++ backend/myproject/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.myproject.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
