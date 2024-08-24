809d4bd01af09de8314d1c0225f1eddf6eb1a2d5
luis-suarez-cuartas <luissuarezcuartas@opendeusto.es>
2024-08-24 19:14:29 +0200

    corregir error import

--- backend/myproject/wsgi.py
+++ backend/myproject/wsgi.py
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import sys
+import os
+from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
 
-import os
 
-from django.core.wsgi import get_wsgi_application
+
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
