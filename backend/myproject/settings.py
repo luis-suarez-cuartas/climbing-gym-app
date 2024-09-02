3b7ba7454c9f90d746858c59c217ff77f07ffde4
luis-suarez-cuartas <luissuarezcuartas@opendeusto.es>
2024-09-02 18:15:00 +0200

    mod django extensions

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'django_extensions',
     'authentication',
     'training',
     'publication',
